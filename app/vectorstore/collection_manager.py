f4ca1d9f94bdbc131de54094ed0bf53401586900
Jason <lee.jasonhk@gmail.com>
2026-09-07 17:17:09 +0800

    add cache for chroma vector store

--- app/vectorstore/collection_manager.py
+++ app/vectorstore/collection_manager.py
@@ -15,8 +15,13 @@ class Collection_manager():
             self.client = client or get_chroma_db().client
             self.embedding_function = EmbeddingFactory.get_embedding_function()
             self.partition_strategy = vector_partition_strategy(settings.PARTITION_METHOD)
-    
+            self.cache: dict[str, Chroma] = {}
+    def _collection_name(self, user_id:str, tenant_id:str) -> str:
+         return f"{settings.COLLECTION_NAME}_{user_id}_{tenant_id}"
     def get_or_create_collection(self, user_id:str, tenant_id:str) -> Chroma:
+        collection_name = self._collection_name(user_id, tenant_id)
+        if collection_name in self.cache:
+            return self.cache[collection_name]
         if self.partition_strategy == vector_partition_strategy.ONE_USER_ONE_COLLECTION:
             vector_store = Chroma(
             client=self.client,
@@ -35,5 +40,6 @@ class Collection_manager():
                 collection_name=f'{settings.COLLECTION_NAME}',
                 embedding_function=self.embedding_function,
             )
+        self.cache[collection_name] = vector_store
         return vector_store
 
